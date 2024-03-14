5a4b119b6fdcd61187ac6573f6cbc0782e9afca5
Joseph Kanzaveli <youngyotheb@gmail.com>
2024-03-14 19:55:09 +0200

    update:

--- cloudbees/set_up.sh
+++ cloudbees/set_up.sh
@@ -1,12 +1,27 @@
+#! REQUIREMENTS: 
+#* EFS.CSI driver
+#* StorageClass connected to AWS EFS
+#* Postgress DataBase 
+
 # Pull CloudBees CD helm chart
 helm repo add cloudbees https://public-charts.artifacts.cloudbees.com/repository/public/
 helm repo update
 
+# Output alues file
+#! CHANGE Values:
+#! Database: Name, Credentials, Port, myaqlconnector=false
+#! Old ingress: Disable
+#! StorageClass: Name
+helm inspect values cloudbees/cloudbees-flow > cd.yaml
+
 # Create NS for CloudBees
-kubectl create ns cludbees
+kubectl create ns cloudbees
 
 # Install CloudBees chart
-helm install cb_cd_demo cloudbees/cloudbees-flow \
-      -f cb_cd_demo.yaml \
+helm install cloudbees cloudbees/cloudbees-flow \
+      -f cd.yaml \
       --namespace cludbees \
       --timeout 1800s
+
+# Get password
+# kubectl get secret --namespace cludbees cloudbees-cloudbees-flow-credentials -o jsonpath="{.data.CBF_SERVER_ADMIN_PASSWORD}" | base64 --decode; echo
